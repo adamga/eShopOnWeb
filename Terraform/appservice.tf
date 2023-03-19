cf882af6b3e96d6da0749db19bb65090173f1d3e
adamga <adamga@microsoft.com>
2023-03-19 14:07:32 -0400

    Update appservice.tf

--- Terraform/appservice.tf
+++ Terraform/appservice.tf
@@ -13,8 +13,8 @@ resource "azurerm_resource_group" "webshopdemo" {
 
 resource "azurerm_app_service_plan" "webshopdemo" {
     name                = "ageshopwebdevServicePlan"
-    location            = azurerm_resource_group.slotDemo.location
-    resource_group_name = azurerm_resource_group.slotDemo.name
+    location            = azurerm_resource_group.webshopdemo.location
+    resource_group_name = azurerm_resource_group.webshopdemo.name
     sku {
         tier = "Standard"
         size = "S1"
@@ -23,9 +23,9 @@ resource "azurerm_app_service_plan" "webshopdemo" {
 
 resource "azurerm_app_service" "webshopdemo" {
     name                = "ageshopwebdevAppService"
-    location            = azurerm_resource_group.slotDemo.location
-    resource_group_name = azurerm_resource_group.slotDemo.name
-    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
+    location            = azurerm_resource_group.webshopdemo.location
+    resource_group_name = azurerm_resource_group.webshopdemo.name
+    app_service_plan_id = azurerm_app_service_plan.webshopdemo.id
 }
 
 data "azurerm_log_analytics_workspace" "webshopdemo" {
@@ -33,61 +33,33 @@ data "azurerm_log_analytics_workspace" "webshopdemo" {
   resource_group_name = "ageshopwebdev"
 }
 
-module "mssql-server" {
-  source  = "kumarvna/mssql-db/azurerm"
-  version = "1.3.0"
 
-  # By default, this module will create a resource group
-  # proivde a name to use an existing resource group and set the argument 
-  # to `create_resource_group = false` if you want to existing resoruce group. 
-  # If you use existing resrouce group location will be the same as existing RG.
-  create_resource_group = false
-  resource_group_name   = "ageshopwebdev"
-  location              = "canadacentral"
+resource "azurerm_sql_server" "webshopdemo" {
+  name                         = "agwebshopdemo-sqlsvr"
+  resource_group_name          = azurerm_resource_group.webshopdemo.name
+  location                     = azurerm_resource_group.webshopdemo.location
+  version                      = "12.0"
+  administrator_login          = "webshopdemoadmin"
+  administrator_login_password = "P2ssw0rd1234!!"
+}
 
-  # SQL Server and Database details
-  # The valid service objective name for the database include S0, S1, S2, S3, P1, P2, P4, P6, P11 
-  sqlserver_name               = "webshopdemodbserver01"
-  database_name                = "webshopdemosqldb"
-  sql_database_edition         = "Standard"
-  sqldb_service_objective_name = "S1"
+resource "azurerm_sql_database" "webshopdemo" {
+  name                             = "agwebshopdemo-db"
+  resource_group_name              = azurerm_resource_group.webshopdemo.name
+  location                         = azurerm_resource_group.webshopdemo.location
+  server_name                      = azurerm_sql_server.webshopdemo.name
+  edition                          = "Basic"
+  collation                        = "SQL_Latin1_General_CP1_CI_AS"
+  create_mode                      = "Default"
+  requested_service_objective_name = "Basic"
+}
 
-  # SQL server extended auditing policy defaults to `true`. 
-  # To turn off set enable_sql_server_extended_auditing_policy to `false`  
-  # DB extended auditing policy defaults to `false`. 
-  # to tun on set the variable `enable_database_extended_auditing_policy` to `true` 
-  # To enable Azure Defender for database set `enable_threat_detection_policy` to true 
-  enable_threat_detection_policy = true
-  log_retention_days             = 30
-
-  # schedule scan notifications to the subscription administrators
-  # Manage Vulnerability Assessment set `enable_vulnerability_assessment` to `true`
-  enable_vulnerability_assessment = false
-  email_addresses_for_alerts      = ["adamga@microsoft.com"]
-
-  # AD administrator for an Azure SQL server
-  # Allows you to set a user or group as the AD administrator for an Azure SQL server
-  ad_admin_login_name = "adamga@adamgallant.net"
-
-  # (Optional) To enable Azure Monitoring for Azure SQL database including audit logs
-  # Log Analytic workspace resource id required
-  # (Optional) Specify `storage_account_id` to save monitoring logs to storage. 
-  enable_log_monitoring      = true
-  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.example.id
-
-  # Firewall Rules to allow azure and external clients and specific Ip address/ranges. 
-  enable_firewall_rules = true
-  firewall_rules = [
-    {
-      name             = "access-to-azure"
-      start_ip_address = "0.0.0.0"
-      end_ip_address   = "0.0.0.0"
-    },
-    {
-      name             = "desktop-ip"
-      start_ip_address = "49.204.225.49"
-      end_ip_address   = "49.204.225.49"
-    }
-  ]
-
-
+# Enables the "Allow Access to Azure services" box as described in the API docs
+# https://docs.microsoft.com/en-us/rest/api/sql/firewallrules/createorupdate
+resource "azurerm_sql_firewall_rule" "webshopdemo" {
+  name                = "allow-azure-services"
+  resource_group_name = azurerm_resource_group.webshopdemo.name
+  server_name         = azurerm_sql_server.webshopdemo.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
