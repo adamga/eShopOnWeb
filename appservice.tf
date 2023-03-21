5c434b690084d017b26634edfae6c826427d63eb
Adam Gallant <adamga@microsoft.com>
2023-03-21 06:48:48 -0400

    sdf

--- appservice.tf
+++ appservice.tf
@@ -6,107 +6,142 @@ provider "azurerm" {
     features {}
 }
 
-data "azurerm_key_vault" "AppDeploymentKV" {
+data "azurerm_key_vault" "mykv" {
   name                = "AppDeploymentKV"
   resource_group_name = "Deploy"
 }
 
-data "azurerm_key_vault_secret" "webshopdemo" {
+data "azurerm_key_vault_secret" "kvrgname" {
   name         = "rgname"
-  key_vault_id = data.azurerm_key_vault.AppDeploymentKV.id
+  key_vault_id = data.azurerm_key_vault.mykv.id
 }
-data "azurerm_key_vault_secret" "webshopdemo" {
+data "azurerm_key_vault_secret" "kvlocation" {
   name         = "dev-ageswrglocation"
-  key_vault_id = data.azurerm_key_vault.dev-ageswrglocation.id
+  key_vault_id = data.azurerm_key_vault.mykv.id
 }
 
-data "azurerm_key_vault_secret" "webshopdemo" {
+data "azurerm_key_vault_secret" "kvasp" {
   name         = "dev-ageswaspname"
-  key_vault_id = data.azurerm_key_vault.dev-ageswaspname.id
+  key_vault_id = data.azurerm_key_vault.mykv.id
 }
 
-data "azurerm_key_vault_secret" "webshopdemo" {
+data "azurerm_key_vault_secret" "kvtier" {
   name         = "dev-ageswasptier"
-  key_vault_id = data.azurerm_key_vault.dev-ageswasptier.id
+  key_vault_id = data.azurerm_key_vault.mykv.id
 }
 
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-dev-ageswasptier"
-  key_vault_id = data.azurerm_key_vault.dev-dev-ageswasptier.id
+data "azurerm_key_vault_secret" "kvsize" {
+  name         = "dev-ageswaspsize"
+  key_vault_id = data.azurerm_key_vault.mykv.id
 }
 
-data "azurerm_key_vault_secret" "webshopdemo" {
+data "azurerm_key_vault_secret" "kvwebname" {
   name         = "dev-ageswapservicename"
-  key_vault_id = data.azurerm_key_vault.dev-ageswapservicename.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswsqlservername"
-  key_vault_id = data.azurerm_key_vault.dev-ageswsqlservername.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswsqlversion"
-  key_vault_id = data.azurerm_key_vault.dev-ageswsqlversion.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswsqladministrator-login"
-  key_vault_id = data.azurerm_key_vault.dev-ageswsqladministrator-login.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswsqladministrator-login-password"
-  key_vault_id = data.azurerm_key_vault.dev-ageswsqladministrator-login-password.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswdbname"
-  key_vault_id = data.azurerm_key_vault.dev-ageswdbname.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswdbedition"
-  key_vault_id = data.azurerm_key_vault.dev-ageswdbedition.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswdbcollation"
-  key_vault_id = data.azurerm_key_vault.dev-ageswdbcollation.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswdbcreate-mode"
-  key_vault_id = data.azurerm_key_vault.dev-ageswdbcreate-mode.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswdbrequested_service_objective_name"
-  key_vault_id = data.azurerm_key_vault.dev-ageswdbrequested_service_objective_name.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswdbsecrulename"
-  key_vault_id = data.azurerm_key_vault.dev-ageswdbsecrulename.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswstart-ip-address"
-  key_vault_id = data.azurerm_key_vault.dev-ageswstart-ip-address.id
-}
-data "azurerm_key_vault_secret" "webshopdemo" {
-  name         = "dev-ageswend-ip-address"
-  key_vault_id = data.azurerm_key_vault.dev-ageswend-ip-address.id
+  key_vault_id = data.azurerm_key_vault.mykv.id
 }
 
+
+data "azurerm_key_vault_secret" "kvsqlserver" {
+  name         = "dev-ageswsqlservername"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvsqlver" {
+  name         = "dev-ageswsqlversion"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvlogin" {
+  name         = "dev-ageswsqladministrator-login"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvpass" {
+  name         = "dev-ageswsqladministrator-login-password"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvdb" {
+  name         = "dev-ageswdbname"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvedition" {
+  name         = "dev-ageswdbedition"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvcollation" {
+  name         = "dev-ageswdbcollation"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvmode" {
+  name         = "dev-ageswdbcreate-mode"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvobj" {
+  name         = "dev-ageswdbrequested_service_objective_name"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvrule" {
+  name         = "dev-ageswdbsecrulename"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvstartip" {
+  name         = "dev-ageswstart-ip-address"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "kvendip" {
+  name         = "dev-ageswend-ip-address"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+
+
+#data "azurerm_key_vault" "example" {
+#  name                = "mykeyvault"
+#  resource_group_name = "myresourcegroup"
+#}
+
+#data "azurerm_key_vault_secret" "secret1" {
+#  name         = "mysecret1"
+#  key_vault_id = data.azurerm_key_vault.example.id
+#}
+
+#data "azurerm_key_vault_secret" "secret2" {
+#  name         = "mysecret2"
+#  key_vault_id = data.azurerm_key_vault.example.id
+#}
+
+#resource "azurerm_virtual_machine" "example" {
+#  # ...
+
+#  os_profile_linux_config {
+#    # ...
+#    ssh_keys {
+#      path     = "/home/user/.ssh/authorized_keys"
+#      key_data = data.azurerm_key_vault_secret.secret1.value
+#    }
+#  }
+
+#  connection {
+#    type     = "ssh"
+#    user     = "user"
+#    password = data.azurerm_key_vault_secret.secret2.value
+#  }
+#}
 
 
 resource "azurerm_resource_group" "webshopdemo" {
-    name = data.azurerm_key_vault_secret.rgname.value
+    name = data.azurerm_key_vault_secret.kvrgname.value
     location =  data.azurerm_key_vault_secret.dev-ageswrglocation.value
 }
 
 resource "azurerm_app_service_plan" "webshopdemo" {
-    name                = data.azurerm_key_vault_secret.dev-ageswaspname.value
+    name                = data.azurerm_key_vault_secret.kvasp.value
     location            = azurerm_resource_group.webshopdemo.location
     resource_group_name = azurerm_resource_group.webshopdemo.name
     sku {
-        tier = data.azurerm_key_vault_secret.dev-ageswasptier.value
-        size =  data.azurerm_key_vault_secret.dev-ageswaspsize.value
+        tier = data.azurerm_key_vault_secret.kvtier.value
+        size =  data.azurerm_key_vault_secret.kvsize.value
     }
 }
 
 resource "azurerm_app_service" "webshopdemo" {
-    name                =  data.azurerm_key_vault_secret.dev-ageswapservicename.value
+    name                =  data.azurerm_key_vault_secret.kvwebname.value
     location            = azurerm_resource_group.webshopdemo.location
     resource_group_name = azurerm_resource_group.webshopdemo.name
     app_service_plan_id = azurerm_app_service_plan.webshopdemo.id
@@ -114,32 +149,32 @@ resource "azurerm_app_service" "webshopdemo" {
 
 
 resource "azurerm_sql_server" "webshopdemo" {
-  name                         =  data.azurerm_key_vault_secret.dev-ageswsqlservername.value
+  name                         =  data.azurerm_key_vault_secret.kvsqlserver.value
   resource_group_name          = azurerm_resource_group.webshopdemo.name
   location                     = azurerm_resource_group.webshopdemo.location
-  version                      =  data.azurerm_key_vault_secret.dev-ageswsqlversion.value
-  administrator_login          =  data.azurerm_key_vault_secret.dev-ageswsqladministrator-login.value
-  administrator_login_password =  data.azurerm_key_vault_secret.dev-ageswsqladministrator-login-password.value
+  version                      =  data.azurerm_key_vault_secret.kvsqlver.value
+  administrator_login          =  data.azurerm_key_vault_secret.kvlogin.value
+  administrator_login_password =  data.azurerm_key_vault_secret.kvpass.value
 }
 
 resource "azurerm_sql_database" "webshopdemo" {
-  name                             = data.azurerm_key_vault_secret.dev-ageswdbname.value
+  name                             = data.azurerm_key_vault_secret.kvdb.value
   resource_group_name              = azurerm_resource_group.webshopdemo.name
   location                         = azurerm_resource_group.webshopdemo.location
   server_name                      = azurerm_sql_server.webshopdemo.name
-  edition                          = data.azurerm_key_vault_secret.dev-ageswdbedition.value
-  collation                        =  data.azurerm_key_vault_secret.dev-ageswdbcollation.value
-  create_mode                      =  data.azurerm_key_vault_secret.dev-ageswdbcreate-mode.value
-  requested_service_objective_name =  data.azurerm_key_vault_secret.dev-ageswdbrequested_service_objective_name.value
+  edition                          = data.azurerm_key_vault_secret.kvedition.value
+  collation                        =  data.azurerm_key_vault_secret.kvcollation.value
+  create_mode                      =  data.azurerm_key_vault_secret.kvmode.value
+  requested_service_objective_name =  data.azurerm_key_vault_secret.kvobj.value
 }
 
 # Enables the "Allow Access to Azure services" box as described in the API docs
 # https://docs.microsoft.com/en-us/rest/api/sql/firewallrules/createorupdate
 resource "azurerm_sql_firewall_rule" "webshopdemo" {
-  name                = data.azurerm_key_vault_secret.dev-ageswdbsecrulename.value
+  name                = data.azurerm_key_vault_secret.kvrule.value
   resource_group_name = azurerm_resource_group.webshopdemo.name
   server_name         = azurerm_sql_server.webshopdemo.name
-  start_ip_address    =  data.azurerm_key_vault_secret.dev-ageswstart-ip-address.value
-  end_ip_address      =  data.azurerm_key_vault_secret.dev-ageswend-ip-address.value
+  start_ip_address    =  data.azurerm_key_vault_secret.kvstartip.value
+  end_ip_address      =  data.azurerm_key_vault_secret.kvendip.value
 }
 
