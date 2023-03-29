fa9dd9c88414356a968770e2dd314662b1d77419
Adam Gallant <adamga@microsoft.com>
2023-03-29 09:18:31 -0400

    asdasd

--- Terraform/aks/aks.tf
+++ Terraform/aks/aks.tf
@@ -14,17 +14,31 @@ data "azurerm_key_vault_secret" "kvlocation" {
   name         = "dev-ageswrglocation"
   key_vault_id = data.azurerm_key_vault.mykv.id
 }
+data "azurerm_key_vault_secret" "acrname" {
+  name         = "dev-eshopwebacr"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "aksname" {
+  name         = "dev-aksname"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+data "azurerm_key_vault_secret" "aksvmsize" {
+  name         = "dev-aksvmsize"
+  key_vault_id = data.azurerm_key_vault.mykv.id
+}
+
+
 
 
 resource "azurerm_container_registry" "webshopaks" {
-  name                = "agwebshopdemocr"
+  name                = data.azurerm_key_vault_secret.acrname.value
   resource_group_name = data.azurerm_key_vault_secret.kvrgname.value
   location            = data.azurerm_key_vault_secret.kvlocation.value
   sku                 = "Basic"
 }
 
 resource "azurerm_kubernetes_cluster" "webshopaks" {
-  name                = "agwebshopcluster"
+  name                = data.azurerm_key_vault_secret.askname.value
   location            = data.azurerm_key_vault_secret.kvlocation.value
   resource_group_name = data.azurerm_key_vault_secret.kvrgname.value
   dns_prefix          = "agwebshopcluster"
@@ -32,7 +46,7 @@ resource "azurerm_kubernetes_cluster" "webshopaks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = data.azurerm_key_vault_secret.aksvmsize.value
   }
 
   identity {
