84056e09460c2591c91271eb268d0afe888a9f30
Adam Gallant <adamga@microsoft.com>
2023-03-30 12:19:14 -0400

    asd

--- aks/akscliinstanll.sh
+++ aks/akscliinstanll.sh
@@ -6,17 +6,15 @@ RESOURCE_GROUP=$2
 
 # Set the release name and chart path
 RELEASE_NAME=$3
-CHART_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/webhelm/eshopwebhelm.yaml"
+#CHART_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/webhelm/eshopwebhelm.yaml"
 
 # Set the values for the variables
 IMAGE_NAME=$4
 CONTAINER_PORT=$5
 SERVICE_PORT=$6
 
-LS
-
 # Connect to the AKS cluster
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME
 
 # Deploy the chart
-helm install $RELEASE_NAME $CHART_PATH --set imageName=$IMAGE_NAME,containerPort=$CONTAINER_PORT,servicePort=$SERVICE_PORTnerPort=$CONTAINER_PORT,servicePort=$SERVICE_PORT
+helm install $RELEASE_NAME $CHART_PATH --set imageName=$IMAGE_NAME,containerPort=$CONTAINER_PORT,servicePort=$SERVICE_PORT
