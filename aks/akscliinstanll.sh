231871431c2c5e542cc86e7b660d4055cee8f9db
Adam Gallant <adamga@microsoft.com>
2023-03-30 10:31:45 -0400

    asd

--- aks/akscliinstanll.sh
+++ aks/akscliinstanll.sh
@@ -1,3 +1,5 @@
+Here is an updated version of the script that sets the CHART_PATH variable to reference a subfolder of the script location called /webhelm and a YAML chart file named eshopwebhelm.yaml:
+
 #!/bin/bash
 
 # Set the AKS cluster name and resource group
@@ -6,15 +8,15 @@ RESOURCE_GROUP=$2
 
 # Set the release name and chart path
 RELEASE_NAME=$3
-CHART_PATH=$4
+CHART_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/webhelm/eshopwebhelm.yaml"
 
 # Set the values for the variables
-IMAGE_NAME=$5
-CONTAINER_PORT=$6
-SERVICE_PORT=$7
+IMAGE_NAME=$4
+CONTAINER_PORT=$5
+SERVICE_PORT=$6
 
 # Connect to the AKS cluster
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME
 
 # Deploy the chart
-helm install $RELEASE_NAME $CHART_PATH --set imageName=$IMAGE_NAME,containerPort=$CONTAINER_PORT,servicePort=$SERVICE_PORT
+helm install $RELEASE_NAME $CHART_PATH --set imageName=$IMAGE_NAME,containerPort=$CONTAINER_PORT,servicePort=$SERVICE_PORTnerPort=$CONTAINER_PORT,servicePort=$SERVICE_PORT
