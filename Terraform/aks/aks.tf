e7fa724ea859e5271b8bdc8437cbcdabcefd3a96
Adam Gallant <adamga@microsoft.com>
2023-04-04 11:00:50 -0400

    zxcxz

--- Terraform/aks/aks.tf
+++ Terraform/aks/aks.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.62.0"
+    }
+  }
+}
+
+
+
+
 provider "azurerm" {
   features {}
 }
