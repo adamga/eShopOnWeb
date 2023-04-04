0e7ed970a1ff8a839575c8e6c05454b47a2acde8
Adam Gallant <adamga@microsoft.com>
2023-04-04 09:01:53 -0400

    asdasd

--- Terraform/aks/aks.tf
+++ Terraform/aks/aks.tf
@@ -54,14 +54,73 @@ resource "azurerm_kubernetes_cluster" "webshopaks" {
   }
 }
 
-
-  resource "helm_release" "csi_secrets_store_provider_azure" {
-  name       = "csi-secrets-store-provider-azure"
-  repository = "https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts"
-  chart      = "csi-secrets-store-provider-azure"
-
-      set {
-        name  = "secrets-store-csi-driver.enabled"
-        value = true
+locals {
+  kv_csi_settings = {
+    linux = {
+      enabled = true
+      resources = {
+        requests = {
+          cpu    = "100m"
+          memory = "100Mi"
+        }
+        limits = {
+          cpu    = "100m"
+          memory = "100Mi"
+        }
       }
+    }
+    secrets-store-csi-driver = {
+      install = true
+      linux = {
+        enabled = true
+      }
+      logLevel = {
+        debug = true
+      }
+    }
   }
+}
+
+
+resource "helm_release" "kv_csi" {
+  name         = "csi-secrets-store-provider-azure"
+  chart        = "csi-secrets-store-provider-azure"
+  version      = "0.0.8"
+  repository   = "https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts"
+  namespace    = "kube-system"
+  max_history  = 4
+  atomic       = true
+  reuse_values = false
+  timeout      = 1800
+  values       = [yamlencode(local.kv_csi_settings)]
+  depends_on = [
+    azurerm_kubernetes_cluster.k8s
+  ]
+
+}
+
+
+#  resource "helm_release" "csi_secrets_store_provider_azure" {
+#  name       = "csi-secrets-store-provider-azure"
+#  repository = "https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts"
+#  chart      = "csi-secrets-store-provider-azure"
+
+#      set {
+#        name  = "secrets-store-csi-driver.enabled"
+#        value = true
+#      }
+#  }
+
+
+#resource "helm_release" "csi_secrets_store_provider_azure" {
+#  name       = "csi-secrets-store-provider-azure"
+#  repository = "https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts"
+#  chart      = "csi-secrets-store-provider-azure"
+
+#  set {
+#    name  = "secrets-store-csi-driver.enabled"
+#    value = true
+#  }
+
+#  depends_on = [azurerm_kubernetes_cluster.webshopaks]
+#}
