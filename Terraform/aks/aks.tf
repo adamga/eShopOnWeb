450bb7fdafeb05ab3cbb9c344346f918f1c3014e
Adam Gallant <adamga@microsoft.com>
2023-04-04 10:42:54 -0400

    asdsa

--- Terraform/aks/aks.tf
+++ Terraform/aks/aks.tf
@@ -48,56 +48,60 @@ resource "azurerm_kubernetes_cluster" "webshopaks" {
     node_count = 1
     vm_size    = data.azurerm_key_vault_secret.aksvmsize.value
   }
-
+  addon_profile {
+    azure_keyvault_secrets_provider {
+      enabled = true
+    }
+  }
   identity {
     type = "SystemAssigned"
   }
 }
 
-locals {
-  kv_csi_settings = {
-    linux = {
-      enabled = true
-      resources = {
-        requests = {
-          cpu    = "100m"
-          memory = "100Mi"
-        }
-        limits = {
-          cpu    = "100m"
-          memory = "100Mi"
-        }
-      }
-    }
-    secrets-store-csi-driver = {
-      install = true
-      linux = {
-        enabled = true
-      }
-      logLevel = {
-        debug = true
-      }
-    }
-  }
-}
+#locals {
+#  kv_csi_settings = {
+#    linux = {
+#      enabled = true
+#      resources = {
+#        requests = {
+#          cpu    = "100m"
+#          memory = "100Mi"
+#        }
+#        limits = {
+#          cpu    = "100m"
+#          memory = "100Mi"
+#        }
+#      }
+#    }
+#    secrets-store-csi-driver = {
+#      install = true
+#      linux = {
+#        enabled = true
+#      }
+#      logLevel = {
+#        debug = true
+#      }
+#    }
+#  }
+#}
 
 
-resource "helm_release" "kv_csi" {
-  name         = "csi-secrets-store-provider-azure"
-  chart        = "csi-secrets-store-provider-azure"
-  version      = "0.0.8"
-  repository   = "https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts"
-  namespace    = "kube-system"
-  max_history  = 4
-  atomic       = true
-  reuse_values = false
-  timeout      = 1800
-  values       = [yamlencode(local.kv_csi_settings)]
-  depends_on = [
-    azurerm_kubernetes_cluster.webshopaks
-  ]
+#resource "helm_release" "kv_csi" {
+#  name         = "csi-secrets-store-provider-azure"
+#  chart        = "csi-secrets-store-provider-azure"
+#  version      = "0.0.8"
+#  repository   = "https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts"
+#  namespace    = "kube-system"
+#  max_history  = 4
+#  atomic       = true
+#  reuse_values = false
+#  timeout      = 1800
+#  values       = [yamlencode(local.kv_csi_settings)]
+#  depends_on = [
+#    azurerm_kubernetes_cluster.webshopaks
+#  ]
 
-}
+#}
 
 
 #  resource "helm_release" "csi_secrets_store_provider_azure" {
