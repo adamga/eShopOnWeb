e22b7afc6ab1c4e874e9ed941c53d9ba79ed5bbe
Adam Gallant <adamga@microsoft.com>
2023-04-05 13:54:12 -0400

    asdasd

--- Terraform/acsaks/main.tf
+++ Terraform/acsaks/main.tf
@@ -43,59 +43,115 @@ data "azurerm_key_vault_secret" "aksvmsize" {
 }
 
 
-
-
-// Naming
-variable "name" {
-  type        = string
-  description = "Location of the azure resource group."
-  default     = data.azurerm_key_vault_secret.kvrgname.value
-}
-
-variable "acrname" {
-  type        = string
-  description = "name of the ACR"
-  default     = data.azurerm_key_vault_secret.acrname.value
+locals {
+  name = data.azurerm_key_vault_secret.kvrgname.value
+  acrname = data.azurerm_key_vault_secret.acrname.value
+  environment = "dev"
+  location = data.azurerm_key_vault_secret.kvlocation.value
+  node_count = 3
+  node_type = data.azurerm_key_vault_secret.aksvmsize.value
+  dns_prefix = data.azurerm_key_vault_secret.aksname.value
 }
 
 
-variable "environment" {
-  type        = string
-  description = "Name of the deployment environment"
-  default     = "dev"
-}
+#// Node type information
 
-// Resource information
+#variable "node_count" {
+#  type        = number
+#  description = "The number of K8S nodes to provision."
+#  default     = 3
+#}
 
-variable "location" {
-  type        = string
-  description = "Location of the azure resource group."
-  default     = data.azurerm_key_vault_secret.kvlocation.value
-}
+#variable "node_type" {
+#  type        = string
+#  description = "The size of each node."
+#  default     = data.azurerm_key_vault_secret.aksvmsize.value
+#}
 
-// Node type information
+#variable "dns_prefix" {
+#  type        = string
+#  description = "DNS Prefix"
+#  default     = data.azurerm_key_vault_secret.aksname.value
 
-variable "node_count" {
-  type        = number
-  description = "The number of K8S nodes to provision."
-  default     = 3
-}
+#// Naming
+#variable "name" {
+#  type        = string
+#  description = "Location of the azure resource group."
+#  default     = data.azurerm_key_vault_secret.kvrgname.value
+#}
 
-variable "node_type" {
-  type        = string
-  description = "The size of each node."
-  default     = data.azurerm_key_vault_secret.aksvmsize.value
-}
+#variable "acrname" {
+#  type        = string
+#  description = "name of the ACR"
+#  default     = data.azurerm_key_vault_secret.acrname.value
+#}
 
-variable "dns_prefix" {
-  type        = string
-  description = "DNS Prefix"
-  default     = data.azurerm_key_vault_secret.aksname.value
-}
+
+#variable "environment" {
+#  type        = string
+#  description = "Name of the deployment environment"
+#  default     = "dev"
+#}
+
+#// Resource information
+
+#variable "location" {
+#  type        = string
+#  description = "Location of the azure resource group."
+#  default     = data.azurerm_key_vault_secret.kvlocation.value
+#}
+
+#}
 
 
 # The main resource group for this deployment
 resource "azurerm_resource_group" "default" {
   name     = var.name
   location = var.location
+}
+
+
+resource "azurerm_container_registry" "default" {
+  name                     = var.acrname
+  resource_group_name      = azurerm_resource_group.default.name
+  location                 = azurerm_resource_group.default.location
+  sku                      = "Standard"
+  admin_enabled            = false
+}
+resource "azurerm_user_assigned_identity" "aks" {
+  location            = azurerm_resource_group.default.location
+  name                = "askidentityname"
+  resource_group_name = azurerm_resource_group.default.name
+}
+
+resource "azurerm_role_assignment" "aks_network" {
+  scope                = azurerm_resource_group.default.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+}
+
+resource "azurerm_role_assignment" "aks_acr" {
+  scope                = azurerm_container_registry.default.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+}
+resource "azurerm_kubernetes_cluster" "default" {
+  name                              = var.aksname
+  location                          = azurerm_resource_group.default.location
+  resource_group_name               = azurerm_resource_group.default.name
+  dns_prefix                        = var.aksname
+  role_based_access_control_enabled = true
+
+  default_node_pool {
+    name            = "default"
+    vm_size         = var.node_type
+    node_count      = var.node_count
+    os_disk_size_gb = 30
+  }
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aks.id]
+  }
+
+  depends_on                        = [azurerm_role_assignment.aks_network, azurerm_role_assignment.aks_acr]
 }
